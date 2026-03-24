a494131c72c57019510d62d5ea7ffa0dbeb5b837
Luis Miguel Paradela Delgado <luismi97_@hotmail.com>
2026-03-24 20:45:35 +0000

    Feature: add lead and sector with controllers, services and repositories

---
+++ MarketingAPI.Core/Services/SectorService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MarketingAPI.Core.Entities;
+using MarketingAPI.Core.Interfaces;
+
+namespace MarketingAPI.Core.Services
+{
+    public class SectorService
+    {
+        private readonly ISectorRepository _repository;
+
+        public SectorService(ISectorRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<List<Sector>> GetAllAsync()
+        {
+            return _repository.GetAllAsync();
+        }
+
+        public Task<Sector?> GetByIdAsync(int id)
+        {
+            return _repository.GetByIdAsync(id);
+        }
+
+        public Task AddSectorAsync(Sector sector)
+        {
+            return _repository.AddAsync(sector);
+        }
+    }
+}
