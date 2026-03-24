582774bd8a83dec1d533700c012c01c92034de71
Luis Miguel Paradela Delgado <luismi97_@hotmail.com>
2026-03-24 20:16:20 +0000

    Add Lead and Sector entities

---
+++ MarketingAPI.Core/Entities/Sector.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketingAPI.Core.Entities
+{
+    public class Sector
+    {
+        public int SectorId { get; set; }
+        public Dictionary<string, string> Names { get; set; }
+        public Dictionary<string, string> Offers { get; set; }
+    }
+}
