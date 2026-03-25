e3f3abd76ad14a3db4bd788717560f1700fbc3e7
Luis Miguel Paradela Delgado <luismi97_@hotmail.com>
2026-03-25 00:37:11 +0000

    feat: generacion dinamica de PDFs de dossiers comerciales usando QuestPDF

---
+++ MarketingAPI.Core/Interfaces/IPdfGeneratorService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketingAPI.Core.Interfaces
+{
+    public interface IPdfGeneratorService
+    {
+        Task<byte[]> GenerateDossierAsync(int leadId, string language);
+    }
+}
